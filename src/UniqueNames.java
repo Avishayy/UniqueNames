3eff92e3593f06a653b9ccbe9edb025f666b880e
Avishay <avikillu@gmail.com>
2016-04-26 17:18:21 +0300

    Made code slightly more modular.

--- src/UniqueNames.java
+++ src/UniqueNames.java
@@ -20,45 +20,12 @@ public class UniqueNames {
         // TAKING CARE OF MIDDLE NAMES STARTS HERE
         // the whole section takes out the middle names from the input and puts them at the array below, names are modified so they won't contain any middle names.
         String[] middleNames = {"", "", ""}; // 0 is billing name, 1 is shipping name, 2 is name on card
-
-        if (fBillFirstName.contains(" ")) { // in case of a middle name at a first name, it'll begin with the first name
-            String[] names = fBillFirstName.split(" ");
-            fBillFirstName = names[0];
-
-            for (int i = 1; i < names.length; ++i) // perhaps someone will come with a middle name that is "The Legend", so preparing for middle names with more than one word
-                middleNames[0] += names[i] + " "; // and yes, "TheLegend" is not the same middle nam as "The Legend"
-        }
-
-        if (fShipFirstName.contains(" ")) {
-            String[] names = fShipFirstName.split(" ");
-            fShipFirstName = names[0];
-
-            for (int i = 1; i < names.length; ++i)
-                middleNames[1] += names[i] + " ";
-        }
-
-        if (fBillLastName.contains(" ")) { // same for last names, where they might drop the middle name at the beginning
-            String[] names = fBillLastName.split(" ");
-            fBillLastName = names[names.length - 1];
-
-            for (int i = 0; i < names.length - 1; ++i)
-                middleNames[0] += names[i] + " ";
-        }
-
-        if (fShipLastName.contains(" ")) {
-            String[] names = fShipLastName.split(" ");
-            fShipLastName = names[names.length - 1];
-
-            for (int i = 0; i < names.length - 1; ++i)
-                middleNames[1] += names[i] + " ";
-        }
-
-        if (fBillNameOnCard.split(" ").length > 2) {
-            String[] names = fBillNameOnCard.split(" ");
-
-            for (int i = 1; i < names.length - 1; ++i)
-                middleNames[2] += names[i] + " ";
-        }
+                                             // type - 0 = firstname, 1 = lastname, 2 = fullname
+        fBillFirstName = updateMiddleNames(fBillFirstName, 0, 0, middleNames);
+        fBillLastName = updateMiddleNames(fBillLastName , 1, 0, middleNames);
+        fShipFirstName = updateMiddleNames(fShipFirstName, 0, 1, middleNames);
+        fShipLastName = updateMiddleNames(fShipLastName, 1, 1, middleNames);
+        fBillNameOnCard = updateMiddleNames(fBillNameOnCard, 2, 2, middleNames);
         // TAKING CARE OF MIDDLE NAMES ENDS HERE
 
         int count = 1;
@@ -67,6 +34,18 @@ public class UniqueNames {
         if ((!isNameOnCard(fBillFirstName, fBillLastName, fBillNameOnCard) && !isNameOnCard(fShipFirstName, fShipLastName, fBillNameOnCard)) || (!equalMiddleNames(middleNames[0], middleNames[2]) || !equalMiddleNames(middleNames[1], middleNames[2])))
             ++count;
         return count;
+    }
+
+    public static String updateMiddleNames(String name, int type, int arrayIndex, String[] arr) { // type - 0 = firstname, 1 = lastname, 2 = fullname
+        String[] names = name.split(" ");
+
+        if (type != 2) // if it's not a card
+            name = names[type == 0 ? 0 : names.length - 1];
+
+        for (int i = (type != 1) ? 1 : 0; i < names.length - ((type != 0) ? 1 : 0); ++i)
+            arr[arrayIndex] += names[i] + " ";
+
+        return name;
     }
 
     public static boolean equalMiddleNames(String a, String b) { // If no middle name is found, it can be related as an equal to any middle name
@@ -96,7 +75,7 @@ public class UniqueNames {
     }
 
     public static boolean isANicknameOf(String nickname, String original) { // TAKE NOTE that 'original' is the first(index) name on a line, a nickname might be any of the names at the same line
-        // loadNames(); // added to apply tests, uncomment if you want to test
+        //loadNames(); // added to apply tests, uncomment if you want to test
         if (nickname.equals(original))
             return true;
         try {
